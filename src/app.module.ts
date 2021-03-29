572f317d2fe29ef0a4a7c011f924cc441dcbb8ed
Torsten Kunkel <git@tkunkel.de>
2021-03-29 20:38:34 +0200

    * printer service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import {PrinterService} from "./printer.service";
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrinterService],
 })
 export class AppModule {}
